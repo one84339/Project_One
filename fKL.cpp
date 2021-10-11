3a57bf6cf41daecc7ce40950b92284fc2d0943e2
Nimesh Vashistha <43320077+nimeshvashistha@users.noreply.github.com>
2021-10-11 14:29:56 +0530

    Create fKL.cpp

---
+++ fKL.cpp
@@ -0,0 +1,11 @@
+int fkL(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q;
+        
+        for(auto it: nums){
+            q.push(it);
+            if(q.size()>k){
+                q.pop();
+            }
+        }
+        return q.top();
+    }
